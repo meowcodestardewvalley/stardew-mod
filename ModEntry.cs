7137eacc104b26676d5c6b6c3e4e0e741d93d567
meowcodestardewvalley <modfarmstardewvalley@gmail.com>
2025-09-04 13:31:27 +0700

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,9 +1,4 @@
 using StardewModdingAPI;
-using StardewModdingAPI.Events;
-using StardewValley;
-using StardewValley.TerrainFeatures;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
 
 namespace AutoTreeChop
 {
@@ -11,28 +6,7 @@ namespace AutoTreeChop
     {
         public override void Entry(IModHelper helper)
         {
-            helper.Events.GameLoop.DayStarted += OnDayStarted;
-        }
-
-        private void OnDayStarted(object sender, DayStartedEventArgs e)
-        {
-            foreach (GameLocation location in Game1.locations)
-            {
-                List<Vector2> toRemove = new List<Vector2>();
-
-                foreach (var pair in location.terrainFeatures.Pairs)
-                {
-                    if (pair.Value is Tree tree && tree.growthStage.Value >= Tree.treeStage)
-                    {
-                        toRemove.Add(pair.Key);
-                    }
-                }
-
-                foreach (var pos in toRemove)
-                    location.terrainFeatures.Remove(pos);
-            }
-
-            Monitor.Log("Đã tự động chặt cây!", LogLevel.Info);
+            Monitor.Log("AutoTreeChop mod loaded!", LogLevel.Info);
         }
     }
 }
